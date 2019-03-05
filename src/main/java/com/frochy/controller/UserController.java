b56a7874db84eb49e60f42f7e695ecffd78fe79e
Frochy <ztyzqq@outlook.com>
2019-03-05 17:10:37 +0800

    增加查找不存在用户为空的判断

--- src/main/java/com/frochy/controller/UserController.java
+++ src/main/java/com/frochy/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.frochy.controller;
 
+import com.frochy.entity.User;
 import com.frochy.service.UserService;
 import com.frochy.util.GSonTool;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +18,20 @@ public class UserController {
 
     @RequestMapping("/getuser")
     @ResponseBody
-    public String getUser(@RequestParam(value="id") Integer id){
-        return GSonTool.userToJson(userService.selectById(id));
+    public String getUser(@RequestParam(value = "id") Integer id) {
+        User user = userService.selectById(id);
+        if (user != null)
+            return GSonTool.userToJson(user);
+        else
+            return "id="+id+"的用户不存在！";
     }
 
     @RequestMapping("/deluser")
     @ResponseBody
-    public String delUser(@RequestParam(value = "id") Integer id){
-        if(userService.deleteById(id)==1)
-            return "成功删除id="+id+"的用户！";
+    public String delUser(@RequestParam(value = "id") Integer id) {
+        if (userService.deleteById(id) == 1)
+            return "成功删除id=" + id + "的用户！";
         else
-            return "id="+id+"的用户不存在！";
+            return "id=" + id + "的用户不存在！";
     }
 }
